5da9c5300d43d15778462a4554d9031f3478e88f
ktsang-cls <Keith.Tsang@theco-operativelegal.co.uk>
2015-04-21 14:11:34 +0100

    Create Program.cs
    Testing a pull

---
+++ Program.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Testing Change
+            Console.WriteLine("Version: " + Environment.Version.ToString());
+            Console.ReadKey();
+        }
+    }
+}
